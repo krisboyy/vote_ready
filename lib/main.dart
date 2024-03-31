0f66c3bff4471254b42d910872b8b2b6cbac0d91
aby pious <abypious3@gmail.com>
2024-03-31 14:08:39 +0530

    dummy

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,11 @@ import 'package:vote_ready/constants.dart';
 import 'package:vote_ready/pages/home_page.dart';
 import 'package:vote_ready/pages/login_page.dart';
 
-import 'components/dialogue.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flame.device.setLandscape();
   Flame.device.fullScreen();
-  runApp(VoteReadyGame());
+  runApp(const VoteReadyGame());
 }
 
 class VoteReadyGame extends StatelessWidget {
